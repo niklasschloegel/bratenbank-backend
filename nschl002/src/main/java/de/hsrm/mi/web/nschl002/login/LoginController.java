d8dd903a56a910c7f66f69683a9b18c519d9a5da
Niklas Schlögel <niklasschloegel@gmail.com>
2020-05-06 19:07:46 +0200

    added span for password hint in template

--- nschl002/src/main/java/de/hsrm/mi/web/nschl002/login/LoginController.java
+++ nschl002/src/main/java/de/hsrm/mi/web/nschl002/login/LoginController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+    private static final String AUTHORIZED_USER = "joghurta";
+    private static final String PW = "joghurta8";
+
     @GetMapping("/login")
     public String firstShow() {
         return "login";
@@ -16,7 +19,7 @@ public class LoginController {
 
     @PostMapping("/login")
     public String login(@RequestParam String username, @RequestParam String password){
-
+        
         return "login";
     }
 }
