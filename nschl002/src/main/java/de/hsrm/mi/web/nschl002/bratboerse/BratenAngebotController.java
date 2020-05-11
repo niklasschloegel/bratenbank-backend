8d05aaf1810a39fd206cb8a7c48532351575c25d
Niklas Schlögel <niklasschloegel@gmail.com>
2020-05-11 19:39:21 +0200

    removed unnecessary imports

--- nschl002/src/main/java/de/hsrm/mi/web/nschl002/bratboerse/BratenAngebotController.java
+++ nschl002/src/main/java/de/hsrm/mi/web/nschl002/bratboerse/BratenAngebotController.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
