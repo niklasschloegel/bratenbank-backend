b30e0f220e20b564b47af02d13163deece3ed3a4
Niklas Schlögel <niklas.schloegel@student.hs-rm.de>
2020-06-08 21:59:29 +0200

    Added REST Api Endpoint for Braten

---
+++ src/main/java/de/hsrm/mi/web/bratenbank/bratapi/BratenApiException.java
@@ -0,0 +1,27 @@
+package de.hsrm.mi.web.bratenbank.bratapi;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BratenApiException extends RuntimeException {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 3423038582702874312L;
+
+    public BratenApiException() {
+    }
+
+    public BratenApiException(String message) {
+        super(message);
+    }
+
+    public BratenApiException(Throwable cause) {
+        super(cause);
+    }
+
+    
+    
+}
---
+++ src/main/java/de/hsrm/mi/web/bratenbank/bratapi/BratenRestApi.java
@@ -0,0 +1,62 @@
+package de.hsrm.mi.web.bratenbank.bratapi;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import de.hsrm.mi.web.bratenbank.bratrepo.Braten;
+import de.hsrm.mi.web.bratenbank.bratservice.BratenService;
+import de.hsrm.mi.web.bratenbank.bratservice.BratenServiceException;
+
+@RestController
+@RequestMapping("/api")
+public class BratenRestApi {
+ 
+    @Autowired BratenService bratenService;
+
+    private Logger logger = LoggerFactory.getLogger(BratenRestApi.class);
+
+    @GetMapping(value="/braten", produces=MediaType.APPLICATION_JSON_VALUE)
+    public List<Braten> getAllBraten(){
+        return bratenService.alleBraten();
+    }
+
+    @GetMapping(value="/braten/{id}", produces=MediaType.APPLICATION_JSON_VALUE)
+    public Braten getBraten(@PathVariable int id) {
+        Optional<Braten> b = bratenService.sucheBratenMitId(id);
+        if (b.isPresent()) return b.get();
+        throw new BratenApiException("Keinen Braten unter der ID "+id+" gefunden");
+    }
+
+    @DeleteMapping(value="/braten/{id}")
+    public void deleteBraten(@PathVariable int id) {
+        Optional<Braten> b = bratenService.sucheBratenMitId(id);
+        if (b.isPresent()) {
+            bratenService.loescheBraten(id);
+        } else {
+            throw new BratenApiException("Es existiert kein Braten mit der ID "+id+".");
+        }
+    }
+
+    @PostMapping(value="/braten", produces=MediaType.APPLICATION_JSON_VALUE)
+    public Braten saveBraten(@RequestParam String loginname, @RequestBody Braten b){
+        try {
+            return bratenService.editBraten(loginname, b);
+        } catch (BratenServiceException bse) {
+            throw new BratenApiException(bse);
+        }
+    }
+
+}
