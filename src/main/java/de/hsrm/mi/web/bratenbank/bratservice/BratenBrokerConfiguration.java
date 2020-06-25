672fc40ef22018af599bedc75c1a8005404e5224
Niklas Schlögel <niklas.schloegel@student.hs-rm.de>
2020-06-25 20:10:37 +0200

    added Broker

--- src/main/java/de/hsrm/mi/web/bratenbank/bratservice/BratenBrokerConfiguration.java
+++ src/main/java/de/hsrm/mi/web/bratenbank/bratservice/BratenBrokerConfiguration.java
@@ -1,5 +1,23 @@
 package de.hsrm.mi.web.bratenbank.bratservice;
 
-public class BratenBrokerConfiguration {
-    
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class BratenBrokerConfiguration implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/stompbroker")
+                .setAllowedOrigins("*");
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.enableSimpleBroker("/topic");
+    }
 }
