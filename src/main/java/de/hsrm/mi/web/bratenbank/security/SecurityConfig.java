e2bddbbf94b20f66957b688930ab0aa06750e9fb
Niklas Schlögel <niklas.schloegel@student.hs-rm.de>
2020-07-01 18:48:59 +0200

    added Security Configuration

---
+++ src/main/java/de/hsrm/mi/web/bratenbank/security/SecurityConfig.java
@@ -0,0 +1,51 @@
+package de.hsrm.mi.web.bratenbank.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration @EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder getPasswordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder pwenc = getPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser("friedfert")
+                .password(pwenc.encode("dingdong"))
+            .and()
+                .withUser("joghurta")
+                .password(pwenc.encode("geheim123"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/benutzer/*").permitAll()
+                .antMatchers("/api/*").permitAll()
+                .antMatchers("/stompbroker").permitAll()
+                .antMatchers("/braten/*").authenticated()
+                .anyRequest().hasRole("ADMIN")
+            .and()
+                .formLogin()
+                .loginPage("login")
+                .defaultSuccessUrl("/braten/angebot")
+                .permitAll()
+            .and()
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .permitAll();
+    }
+}
